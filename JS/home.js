0ed5a7e6bcd21abc555329f17bb39e11f2395d4a
miraz661 <mirazh661@gmail.com>
2023-12-27 17:03:17 +0600

    JSON files added

--- JS/home.js
+++ JS/home.js
@@ -1,5 +1,5 @@
-window.onload = ()=>{
-    if(document.referrer == '' && localStorage.getItem("isLogin") != "true"){
+window.onload = () => {
+    if (document.referrer == '' && localStorage.getItem("isLogin") != "true") {
         self.location = '/html/login.html';
     }
 }
@@ -11,7 +11,7 @@ const body = document.querySelector("#body");
 const acc = document.querySelector(".account");
 const logout = document.querySelector(".logout");
 const main = document.querySelector(".main");
-const backImg = ['../images/background1.jpg','../images/background2.jpg','../images/background3.jpg','../images/background4.jpg','../images/background5.jpg','../images/background6.jpg','../images/background7.jpg','../images/background8.jpg','../images/background9.jpg','../images/background10.jpg'];
+const backImg = ['../images/background1.jpg', '../images/background2.jpg', '../images/background3.jpg', '../images/background4.jpg', '../images/background5.jpg', '../images/background6.jpg', '../images/background7.jpg', '../images/background8.jpg', '../images/background9.jpg', '../images/background10.jpg'];
 const batches = document.querySelectorAll(".batch");
 const addBatch = document.querySelector(".addBatch");
 const addBatchPage = document.querySelector("#addBatches");
@@ -28,59 +28,59 @@ const ConfirmDel = document.querySelector(".delConf");
 const batchInf = document.querySelectorAll(".batInf");
 const notify = document.querySelector(".notification");
 
-acc.addEventListener("click",()=>{
+acc.addEventListener("click", () => {
     logout.classList.toggle("active");
 })
-main.addEventListener("click",()=>{
+main.addEventListener("click", () => {
     logout.classList.remove("active");
 })
-logout.addEventListener("click",()=>{
+logout.addEventListener("click", () => {
     self.location = '../html/login.html';
     localStorage.removeItem("isLogin");
 })
 
-addBatch.addEventListener("click",()=>{
+addBatch.addEventListener("click", () => {
     addBatchPage.classList.toggle("active");
 })
-cancelAddB.addEventListener("click",()=>{
+cancelAddB.addEventListener("click", () => {
     addBatchPage.classList.remove("active");
 })
 
 let batchCount = 1;
 
-createBatch.addEventListener("click",()=>{
-    if(batchNo.value != '' && section.value != ''){
+createBatch.addEventListener("click", () => {
+    if (batchNo.value != '' && section.value != '') {
         batchCount++;
         let batN = document.createElement("div");
         batN.classList.add("batchNo");
         batN.innerText = batchNo.value;
         let batS = document.createElement("div");
         batS.classList.add("section");
-        batS.innerText = "Section - "+section.value;
+        batS.innerText = "Section - " + section.value;
         let del = document.createElement("div");
         del.classList.add("batchOpt");
         let icon = document.createElement("i");
-        icon.classList.add("fa-solid","fa-trash");
+        icon.classList.add("fa-solid", "fa-trash");
         del.appendChild(icon);
         let mainbatch = document.createElement("div");
-        mainbatch.classList.add("batch","batch2");
+        mainbatch.classList.add("batch", "batch2");
         mainbatch.appendChild(batN);
         mainbatch.appendChild(batS);
         mainbatch.appendChild(del);
-        batchBox.insertBefore(mainbatch,batchBox.firstChild);
+        batchBox.insertBefore(mainbatch, batchBox.firstChild);
         addBatchPage.classList.remove("active");
-        let x = Math.round(Math.random()*9);
-        mainbatch.style.backgroundImage = 'url('+backImg[x]+')';
-        
-        notify.innerText = "Batch "+ batchNo.value +" added successfully...";
+        let x = Math.round(Math.random() * 9);
+        mainbatch.style.backgroundImage = 'url(' + backImg[x] + ')';
+
+        notify.innerText = "Batch " + batchNo.value + " added successfully...";
         notify.classList.add("active");
         setTimeout(() => {
             notify.classList.remove("active");
         }, 4000);
-        
+
         batchNo.value = "";
         section.value = "";
-    }else{
+    } else {
         notify.innerText = "Please complete the form...";
         notify.classList.add("active");
         notify.classList.add("wrong");
@@ -94,17 +94,17 @@ createBatch.addEventListener("click",()=>{
 let delEle;
 
 batchDel.forEach(element => {
-    element.addEventListener("click",()=>{
+    element.addEventListener("click", () => {
         delBtn.classList.add("active");
         delEle = element.parentElement;
     })
 });
 
-delCancel.addEventListener("click",()=>{
+delCancel.addEventListener("click", () => {
     delBtn.classList.remove("active");
 })
 
-ConfirmDel.addEventListener("click",()=>{
+ConfirmDel.addEventListener("click", () => {
     delEle.remove();
     notify.innerText = "Batch delete successfully...";
     notify.classList.add("active");
@@ -117,23 +117,52 @@ ConfirmDel.addEventListener("click",()=>{
 })
 
 
-document.addEventListener("DOMContentLoaded",()=>{
-    batches.forEach(element =>{
-        let x = Math.round(Math.random()*9);
-        element.style.backgroundImage = 'url('+backImg[x]+')';
+document.addEventListener("DOMContentLoaded", () => {
+    batches.forEach(element => {
+        let x = Math.round(Math.random() * 9);
+        element.style.backgroundImage = 'url(' + backImg[x] + ')';
     })
 })
 
-batchInf.forEach(element =>{
-    element.addEventListener("click",()=>{
+batchInf.forEach(element => {
+    element.addEventListener("click", () => {
         self.location = '/html/batch.html';
-        localStorage.setItem("batch",element.children[0].innerText);
-        localStorage.setItem("section",element.children[1].children[0].innerText);
+        localStorage.setItem("batch", element.children[0].innerText);
+        localStorage.setItem("section", element.children[1].children[0].innerText);
     })
 })
 
-setInterval(loadPage,3000);
+setInterval(loadPage, 3000);
 
-function loadPage(){
+function loadPage() {
     body.classList.add("body");
 }
+
+
+fetch('../images/data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        json.map(value => {
+            let batch1 = document.createElement("div");
+            batch1.classList.add("batch", "batch1");
+            let batch2 = document.createElement("div");
+            batch2.classList.add("batInf");
+            let batchNo = document.createElement("div");
+            batchNo.classList.add("batchNo");
+            batchNo.innerText = `${value.batchNo}`;
+            let section = document.createElement("div");
+            section.classList.add("section");
+            section.innerText = "Section - " + ` ${value.section}`;
+            batch2.appendChild(batchNo);
+            batch2.appendChild(section);
+            let del = document.createElement("div");
+            del.classList.add("batchOpt");
+            let icon = document.createElement("i");
+            icon.classList.add("fa-solid", "fa-trash");
+            del.appendChild(icon);
+            batch1.appendChild(batch2);
+            batch1.appendChild(del);
+            batchBox.appendChild(batch1);
+            batch1.style.backgroundImage = 'url('+`${value.bgImg}`+')';
+        })
+    });
